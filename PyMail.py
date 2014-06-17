73884bc02c65aa76957989802482f8932ad944e1
jacklam <jacklam718@gmail.com>
2014-06-17 16:16:16 +0800

    Update PyMail.py

--- PyMail.py
+++ PyMail.py
@@ -2,7 +2,7 @@
 # --*-- coding: utf-8 --*--
 #    The MIT License (MIT)
 #
-#    Copyright (c) 2013 Jack Lam
+#    Copyright (c) 2013 Jack Lam <jacklam718@gmail.com>
 #
 #    Permission is hereby granted, free of charge, to any person obtaining a copy
 #    of this software and associated documentation files (the "Software"), to deal
